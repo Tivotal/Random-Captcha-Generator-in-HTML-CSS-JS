f57eb20e4f5238bed8525197b09fce290eaa83b1
Tivotal <121335284+Tivotal@users.noreply.github.com>
2024-07-12 10:17:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,10 +6,14 @@ let refreshBtn = document.querySelector(".refresh-btn");
 let captcha = "";
 
 let generateCaptcha = () => {
-  const randomString = Math.random().toString(36).substring(2, 7);
+  const randomString = Math.random()
+                      .toString(36)
+                      .substring(2, 7);
   const randomStringArray = randomString.split("");
   const changeString = randomStringArray.map((char) =>
-    Math.random() > 0.5 ? char.toUpperCase() : char
+    Math.random() > 0.5 
+    ? char.toUpperCase() 
+    : char
   );
   captcha = changeString.join("   ");
   captchaTxt.value = captcha;
