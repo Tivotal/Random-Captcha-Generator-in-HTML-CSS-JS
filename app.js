f240ea9f01427b5038c8016ca82bcb714b4a3658
Tivotal <121335284+Tivotal@users.noreply.github.com>
2024-07-12 10:16:30 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+/* Created by Tivotal */
+
+let captchaTxt = document.querySelector(".captcha-text");
+let refreshBtn = document.querySelector(".refresh-btn");
+
+let captcha = "";
+
+let generateCaptcha = () => {
+  const randomString = Math.random().toString(36).substring(2, 7);
+  const randomStringArray = randomString.split("");
+  const changeString = randomStringArray.map((char) =>
+    Math.random() > 0.5 ? char.toUpperCase() : char
+  );
+  captcha = changeString.join("   ");
+  captchaTxt.value = captcha;
+};
+
+refreshBtn.addEventListener("click", generateCaptcha);
+generateCaptcha();
